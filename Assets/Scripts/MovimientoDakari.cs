7b1f38d6539c6623e62040594ae501fb4430691a
Ander <anderrod01@gmail.com>
2022-05-16 20:02:00 +0200

    dasda

--- Assets/Scripts/MovimientoDakari.cs
+++ Assets/Scripts/MovimientoDakari.cs
@@ -10,22 +10,47 @@ public class MovimientoDakari : MonoBehaviour
     public float Speed;
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
+    private bool isFacingRight = true;
     private bool Grounded;
     private int health = 5;
+
+    private float activeMoveSpeed;
+
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        activeMoveSpeed = Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Horizontal < 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        else if (Horizontal > 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        if (isDashing)
+        {
+            return;
+        }
+
+
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
+
+       /* if (Horizontal < 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);*/
 
         Debug.DrawRay(transform.position, Vector3.down * 0.25f, Color.cyan);
         if (Physics2D.Raycast(transform.position, Vector3.down, 0.25f))
@@ -36,8 +61,33 @@ public class MovimientoDakari : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.W) && Grounded)
             jump();
 
+
         if (Input.GetKeyDown(KeyCode.F))
             shoot();
+
+        if (Input. GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Horizontal * Speed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && Horizontal > 0f || !isFacingRight && Horizontal < 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 
     private void shoot()
@@ -52,12 +102,9 @@ public class MovimientoDakari : MonoBehaviour
     private void jump()
     {
         Rigidbody2D.AddForce(Vector2.up * JumpForce);
+
     }
 
-    private void FixedUpdate()
-    {
-        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
-    }
 
     public void hit(int potencia)
     {
@@ -72,4 +119,22 @@ public class MovimientoDakari : MonoBehaviour
         if (health == 0) Destroy(gameObject);
     }
 
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        activeMoveSpeed = dashingPower;
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+       
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+        activeMoveSpeed = Speed;
+
+    }
 }
